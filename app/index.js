775a7e74493aca151782738907d4f8ca912eb734
Onvo <Robin.Weymans@gmail.com>
2017-06-18 12:17:22 +0200

    Add images to website
    qlskd

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
@@ -10,6 +11,7 @@ var exec = require('child_process').exec;
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+app.use(express.static(__dirname));
 
 io.on('connection', function(socket){
   console.log('a user connected');
